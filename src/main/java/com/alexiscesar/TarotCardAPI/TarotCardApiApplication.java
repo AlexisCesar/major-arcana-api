08c506ca4511f533f2b9a25105a0bfa6eb649f6b
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-07-24 13:17:17 -0300

    Initial commit

---
+++ src/main/java/com/alexiscesar/TarotCardAPI/TarotCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.alexiscesar.TarotCardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotCardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexiscesar/TarotCardAPI/TarotCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiscesar.TarotCardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
